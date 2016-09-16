6018afb2f9e5fd16ed314920dc16bfc0413e91f8
BarzinM <barzin.m@gmail.com>
2016-09-16 16:23:47 -0400

    tested and validated the download function, changed README

--- README.md
+++ README.md
@@ -3,17 +3,24 @@ Dynamip (WIP)
 
 ### THIS IS A WORK IN PROGRESS
 
-## SSH into Devices Connected to Internet
+## Keep Track of Dynamic IP of Your Remote Devices
 
 This is a tool that helps different devices to know each other's public IPs (static or dynamic) and also local IPs. Using this tool you can SSH into devices over internet without the need for setting up a static IP. It uses local network if the target device is accessible through local network. 
 
 Dynamip uses Google Drive API to communicate IP values between devices. User needs to authorize the tool to connect to Google Drive.
 
 ## As of Now
-You can use the file `syncIPfile.py` to try Dynamip.
+You can use the file `syncIPfile.py` to try Dynamip so far:
+	
+	python3 syncIPfile.py
 
-## TODO:
-A lot.
+## TODO
+
+- [ ] Finish development of main functionalities.
+- [ ] SSH wrapper for forward and reverse tunneling.
+- [ ] Bash scripts for: installation, setup, cronjob
+- [ ] Documentation: dependencies, setup, test
+- [ ] A lot more
 
 ## Contribution
 Very welcome.
--- syncIPfile.py
+++ syncIPfile.py
@@ -1,10 +1,10 @@
-from driveapi import getFileIdFromName, getServiceInstant, updateFile, FileOnDriveError, insertFile
+from driveapi import getFileIdFromName, getServiceInstant, updateFile, FileOnDriveError, insertFile, download_file
 from misc import getIP, getChangedIP, writeIPToFile, readIPFromFile, DynamipError, getHostname, getLocalIP, getHostname
 from json import dump, load
 
 
 def main():
-    """TODOS"""
+    """TODO"""
     file_name = 'Dynamip'
 
     hostname = getHostname()
@@ -16,47 +16,62 @@ def main():
     try:
         print("Finding file on google Drive ...")
         file_id = getFileIdFromName(service, file_name)
+        
         # download it
-        # download .........
+        with open(file_name,'wb+') as file_handle:
+            download_file(service,file_id,file_handle)
+            
         ip_public_saved = readIPFromFile(file_name, hostname)
+        
         print("Done")
+
     except KeyError as e:
         print("An attribute could not be found in %s file: %s" % (file_name, e))
         print("Generating the file ...")
+        
         writeIPToFile(file_name, hostname)
+        
         print("Done.")
+        
         ip_public_saved = ip_public_current
         ip_local_saved = ip_local_current
-    except FileOnDriveError:
-        print("File could not be located on Google Drive.")
+    
+    except FileOnDriveError as e:
+        print("File could not be located on Google Drive:",e)
+        
         # check see if it exists locally
         try:
             print("Looking for the file on disk ...")
+        
             ip_public_saved, ip_local_saved = readIPFromFile(
                 file_name, hostname)
+        
             print("Found it.")
+        
             if ip_public_current != ip_public_saved or ip_local_current != ip_local_saved:
                 print('Old IP was', ip_public_saved,
                       'and current one is', ip_public_current)
+        
                 writeIPToFile(file_name, hostname)
+        
         except (FileNotFoundError, KeyError) as e:
-            print(e)
+            print("An error happend:", e)
             print("Generating the file ...")
+        
             writeIPToFile(file_name, hostname)
+        
             print("Done.")
+        
             ip_public_saved = ip_public_current
             ip_local_saved = ip_local_current
+        
         print("Uploading the file to Google Drive ...")
+        
         file_metadata = insertFile(service, file_name)
         file_id = file_metadata['id']
+        
         print("%s has been uploaded with the id %s" % (file_name, file_id))
 
-    # while True:
-    #     ip_public_current = getChangedIP(.3)
-    #     writeIPToFile(file_name)
-    #     metadata = updateFile(service, file_id, file_name)
-    #     print(metadata)
-    #     dump(metadata, file)
 
 if __name__ == '__main__':
     main()
